09eaf511d4b282398b1ba496bc39f8c402075339
bennycraig <57501435+bennycraig@users.noreply.github.com>
2023-03-10 17:28:14 -0500

    Added functionality to queue song by double-clicking on it. Need to look into Spotify Web Playback SDK for better playback options (such as immediate playback when double clicking)

--- frontend/src/SongRow.js
+++ frontend/src/SongRow.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './SongRow.css'
+import { useDataLayerValue } from './DataLayer';
 
 function SongRow({ track }) {
+    // Get the Spotify API object
+    const [{ spotify }, dispatch] = useDataLayerValue();
+
+    function handleDoubleClick() {
+        spotify.queue(`spotify:track:${track.id}`)
+    }
+
     return (
-        <div className="songRow" >
+        <div className="songRow" onDoubleClick={handleDoubleClick}>
             <img className="songRow__album" src={track.album.images[0].url} alt="" />
             <div className="songRow__info">
                 <h1>{track.name}</h1>
