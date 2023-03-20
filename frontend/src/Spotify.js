5393574d60e7c856ca96b9cdaa90028686207644
bennycraig <57501435+bennycraig@users.noreply.github.com>
2023-03-20 19:48:20 -0400

    Fixed redirect URL for deployment

--- frontend/src/Spotify.js
+++ frontend/src/Spotify.js
@@ -4,7 +4,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Client ID is 'Sound Rover' App on Benny's Spotify Developer account
 const clientId = "b176c89eb06a4faebc76fafc5dda1dca"
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://dev.dzzacllm3y2z3.amplifyapp.com/";
 const scopes = [
     "user-read-currently-playing",
     "user-read-recently-played",
