f8da99bc245b318722900edc65f8f2b8e272f96f
anmastru <anmastru@umich.edu>
2023-03-23 16:23:21 -0400

    Working search bar, some of it might still be buggy

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from '@material-ui/core';
-import SongRow from './SongRow';
 import { useDataLayerValue } from './DataLayer';
 
 function Header() {
@@ -10,54 +9,76 @@ function Header() {
     const [{ user }] = useDataLayerValue();
     const [showMenu, setShowMenu] = useState(false);
     const [{ spotify }, dispatch] = useDataLayerValue();
+    const [searchResults, setSearchResults] = useState([]);
+    const [showResults, setShowResults] = useState(false);
 
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     }
 
+    // Queries Spotify's search API with search term
+    // whenever enter key is detected
     const searchSpotify = () => {
         let input = document.getElementById("search");
         let query = input.value;
         
-        let searchResults = spotify.searchTracks(query, {limit: 10})
+        spotify.searchTracks(query, {limit: 10})
         .then(function(data) {
-            console.log('Search results', data);
-            // for(int i = 0; i < data.tracks.items)
-
-            // }
-            dispatch({
-                type: "SET_SELECTED_MUSIC",
-                selected_music: data.tracks.items
-              })
-            // return (
-            // <div className="body">
-            //     <Header spotify={spotify} />
-    
-            //     <div className="body__songs">
-            //         {/* List of Songs */}
-            //         {data.tracks.items.map(item => (
-            //             <SongRow key={item.id} track={item} />
-            //         ))}
-            //     </div>
-            // </div>
+            console.log('Search results: ', data);
+            setSearchResults(data.tracks.items);
+            setShowResults(true);
         
           }, function(error) {
             console.error(error);
           });
+    }
 
-          
+    const handleInputClick = () => {
+        setShowResults(true);
+    }
+    const handleInputBlur = () => {
+        setTimeout(() => {
+            setShowResults(false);
+        }, 200);
+    }
+
+    const playSong = (uri) => {
+        spotify.play({
+            uris: [uri]
+        })
+        .then(() => {
+            console.log("Playing song with uri: ", uri);
+        }, (err) => {
+            console.error(err);
+        });
     }
 
     return (
         <div className="header">
             <div className="header__left">
                 <SearchIcon />
-                <input id="search" type="text" class="input" placeholder="Search..." onKeyDown={event => {if(event.key === 'Enter') searchSpotify()}}/>
-               
+                <input id="search" type="text" class="input" placeholder="Search..." 
+                    onKeyDown={(event) => {if(event.key === 'Enter') searchSpotify()}}
+                    onClick={handleInputClick}
+                    onBlur={handleInputBlur}
+                />
+                {showResults && searchResults.length > 0 && (
+                    <div className="header__searchResults">
+                        {searchResults.map((result) => (
+                            <div key={result.id} className="header__searchResult" onClick={() => playSong(result.uri)}>
+                                <img src={result.album.images[0].url} alt="" />
+                                <div>
+                                    <h4>{result.name}</h4>
+                                    <p>{result.artists.map((artist) => artist.name).join(', ')}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
 
             <div className="header__right" onClick={toggleMenu}>
-                {/* <Avatar src={user?.images[0].url} alt={user?.display_name} /> */}
+                <Avatar src={user?.images[0].url} alt={user?.display_name} />
                 <h4>{user?.display_name}</h4>
                 {showMenu && (
                     <div className="dropdown">
